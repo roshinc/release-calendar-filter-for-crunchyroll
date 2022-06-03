e2bcea34f041095b5118dd24dba2501cf2823700
Roshin C <idroshin2010@gmail.com>
2022-06-03 00:48:36 -0400

    Added constants for radio group values and indexs

--- ext/src/js/lib/constants.js
+++ ext/src/js/lib/constants.js
@@ -5,15 +5,25 @@ export const CRRS_CLASS = "cr-rs-class";
 export const CRRS_FILTER_MENU_DIV_ID = "cr-rs-filter-menu";
 
 // Show Dubs Toggle
-export const CRRS_FILTER_MENU_SHOW_DUBS_INPUT_ID = "cr-rs-filter-menu-show-dubs";
+// export const CRRS_FILTER_MENU_SHOW_DUBS_INPUT_ID = "cr-rs-filter-menu-show-dubs";
 
 // Dubs Picker
 export const CRRS_FILTER_MENU_PICK_DUBS_DIV_ID = "cr-rs-filter-menu-pick-dubs";
 export const CRRS_FILTER_MENU_PICK_DUBS_INPUT_ID_PREFIX = "cr-rs-filter-menu-pick-dubs-lang-";
 
 // Radio Groups
+export const CRRS_FILTER_MENU_DUBS_RADIO_GROUP_NAME = "dubbed-switch";
 export const CRRS_FILTER_MENU_QUEUE_RADIO_GROUP_NAME = "in-queue-switch";
 export const CRRS_FILTER_MENU_PERMIERE_RADIO_GROUP_NAME = "permiere-switch";
+
+// Radio Group Values & Indexes
+export const CRRS_FILTER_MENU_RADIO_GROUP_ONLY_VALUE = "Only";
+export const CRRS_FILTER_MENU_RADIO_GROUP_ONLY_INDEX = 0;
+export const CRRS_FILTER_MENU_RADIO_GROUP_SHOW_VALUE = "Show";
+export const CRRS_FILTER_MENU_RADIO_GROUP_SHOW_INDEX = 1;
+export const CRRS_FILTER_MENU_RADIO_GROUP_HIDE_VALUE = "Hide";
+export const CRRS_FILTER_MENU_RADIO_GROUP_HIDE_INDEX = 2;
+
 
 // End Button Group
 export const CRRS_FILTER_MENU_LOCK_BTN_ID = "cr-rs-filter-menu-lock-filters";
